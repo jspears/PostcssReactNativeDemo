ee987ff648a9753c7a4aab255490c6bba76e1144
Justin Spears <speajus@gmail.com>
2016-06-07 21:50:05 -0700

    Animations!

--- index.ios.js
+++ index.ios.js
@@ -9,19 +9,39 @@ import {
     AppRegistry,
     StyleSheet,
     Text,
+    Dimensions,
+    LayoutAnimation,
+    TouchableOpacity,
     View
 } from 'react-native';
-import styles from './dist/welcome.css';
-import {View as StyledView, StyleSheet as cs, DimensionComponent, StyledText} from './dist/component.css';
+import Animated from 'Animated';
 
+import styles from './dist/welcome.css';
+import {View as StyledView, StyleSheet as cs, DimensionComponent, SlideIn, StyledText} from './dist/component.css';
+
+
+const test = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    square: {
+        width: 30,
+        height: 30,
+        backgroundColor: 'blue'
+    }
+});
 
 class PostcssReactNativeDemo extends DimensionComponent {
-      render() {
+    render() {
         return (
             <View style={cs.container}>
                 <StyledView className="green">
                     <Text style={cs.text}>Hello</Text>
                     <StyledText>Click Me!</StyledText>
+                    <SlideIn>
+                        <Text>Hello Cruel World!</Text>
+                    </SlideIn>
+
                 </StyledView>
                 <Text style={styles.welcome}>
                     Welcome to React Native!
